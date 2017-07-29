4381a8b73081bca53c86a9546d76fdab064a8335
sang <tony_633@hotmail.com>
2017-07-28 23:46:13 -0400

    added styles

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
 
 app.set('port', (process.env.PORT || 3000));
@@ -14,53 +13,94 @@ app.set('view engine', 'ejs');
 
 // error handling middleware
 app.use(function(err, req, res, next){
-    console.log(err); // to see properties of message in our console
-    res.status(422).send({error: err.message});
+console.log(err); // to see properties of message in our console
+res.status(422).send({error: err.message});
 });
 
 app.get('/', function(req,res){
 	res.render('index');
 })
 
-// app.get('/api/todos', (req, res, next) => {
-// 	Todo.find({}).then(function(todos){
-// 		res.send(todos);
-// 	}).catch(next);
-// });
+app.get('/:date', function(req,res){
+	var date = {
+		unix:0001,
+		natural:"September 11, 1991"
+	}
 
-// app.post('/api/todos', (req,res, next) => {
-// 	console.log("REQ BODY");
-// 	console.log(req.body);
-// 	Todo.create(req.body).then((todo) => {
-// 		//get object put into app
-// 		console.log("Going Back to client");
-// 		res.send(todo);
-// 	}).catch(next);
-// });
+	var arr = req.params.date.split(/[ ,%]+/);
 
-// app.put('/api/todos', (req, res, next) => {
-// 	console.log('update req body');
-// 	console.log(req.body.params);
+	//if length is 1 and is a number then probaby a unix timestamp
+	if (arr.length === 1 && isNaN(arr[0]) === false) {
+		var natural = formatDate(new Date(parseInt(arr[0])));
+		res.json({unix:arr[0], natural:natural})
+	}  
+	else if (arr.length === 3 && validMonth(arr[0]) && validDay(arr[1]) && validYear(arr[2])) {
+		var date = new Date(jsUcfirst(arr[0]) + ' ' +  arr[1] + ', ' + arr[2]);
+		var milli = date.getTime();
 
-// 	Todo.findByIdAndUpdate({_id: req.body.params._id}, req.body.params)
-// 		.then(function(){
-// 			Todo.findOne({_id: req.query.id}).then(function(todo){
-// 				res.send(todo);
-// 			});
-// 		}).catch(next);
-// });
+		res.json({unix:milli,natural:jsUcfirst(arr[0]) + ' ' +  arr[1] + ', ' + arr[2]})
+	} 
+	else {
+		res.json({unix:null,natural:null})
+	}
+})
 
-// app.delete('/api/todos', (req, res, next) => {
-// 	console.log('delete req body');
-// 	console.log(req);
-// 	Todo.findByIdAndRemove({_id: req.query._id}).then(function(todo){
-// 		res.send(todo);
-// 	}).catch(next);
-// });
+
+//Helper functions
+function jsUcfirst(string) 
+{
+	var string = string.toLowerCase();
+	return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function validMonth(input) {
+	var monthNames = [
+		"January", "February", "March",
+		"April", "May", "June", "July",
+		"August", "September", "October",
+		"November", "December", "Jan", 
+		"Feb", "Mar", "Apr", "Aug", 
+		"Sept", "Oct", "Nov", "Dec"
+		];
+
+	if (isNaN(input) && monthNames.includes(jsUcfirst(input))) {
+		return true;
+	}	
+	return false;
+}
+
+function validDay(input) {
+	if (!isNaN(input) && input.length <= 2) {
+		return true;
+	}
+	return false;
+} 
+
+function validYear(input) {
+	if (!isNaN(input) && input.length === 4) {
+		return true;
+	}
+	return false;
+}
+
+function formatDate(date) {
+	var monthNames = [
+	"January", "February", "March",
+	"April", "May", "June", "July",
+	"August", "September", "October",
+	"November", "December"
+	];
+
+	var day = date.getDate();
+	var monthIndex = date.getMonth();
+	var year = date.getFullYear();
+
+	return monthNames[monthIndex] + ' ' +  day + ', ' + year;
+}
 
 
 
 // listen for requests
 app.listen(process.env.port || app.get('port'), () => {
-  console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
+console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
 });
